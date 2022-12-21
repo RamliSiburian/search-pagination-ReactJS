8c1cfd8567b1697f1773b4db604efc9a20f0bd7c
RamliSiburian <ramlisiburian1111@gmail.com>
2022-12-21 12:57:40 +0700

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,16 @@
 import React, { useContext, useState } from 'react'
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/User-context';
 import Login from './modal/auth/Login';
+import * as Icon from 'react-icons/fa'
+import InfoLogin from './modal/info';
 
 
 
 function Navbars() {
     const [showLogin, setShowLogin] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
     const [state, dispatch] = useContext(UserContext)
     const navigate = useNavigate()
 
@@ -29,17 +32,21 @@ function Navbars() {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end fw-bold'>
-                        <Nav className="me-end gap-3">
-                            {state.isLogin ? (
+                        {state.isLogin ? (
+                            <Nav className="me-end gap-3">
                                 <Button className='last fw-bold' onClick={Logout} style={{ cursor: "pointer" }}>LogOut</Button>
-                            ) : (
+                            </Nav>
+                        ) : (
+                            <Nav className="me-end gap-3">
+                                <span onClick={() => setShowInfo(true)}><Icon.FaInfoCircle /></span>
                                 <Button className='last fw-bold' onClick={() => setShowLogin(true)}>Sign In</Button>
-                            )}
-                        </Nav>
+                            </Nav>
+                        )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
             <Login show={showLogin} setShow={setShowLogin} />
+            <InfoLogin show={showInfo} setShow={setShowInfo} />
         </div>
     )
 }
---
+++ src/component/modal/info.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Table } from 'react-bootstrap';
+
+function InfoLogin({ show, setShow }) {
+    const handleClose = () => setShow(false)
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose} size="sm" centered>
+                <Modal.Header closeButton>
+                    <Modal.Title className='modal-title fs-3 fw-bold text-primary' >Info Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table>
+                        <tr>
+                            <th>Username</th>
+                            <th>Password</th>
+                        </tr>
+                        <tr>
+                            <td>Admin</td>
+                            <td>admin123</td>
+                        </tr>
+                        <tr>
+                            <td>Admin</td>
+                            <td>admin123</td>
+                        </tr>
+                    </Table>
+                </Modal.Body>
+            </Modal>
+        </>
+    )
+}
+
+export default InfoLogin;
